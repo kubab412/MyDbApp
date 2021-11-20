49eb4b710361fbd2faee10f6f642ac76e4c50d40
kubab412 <kubab412@gmail.com>
2021-11-20 17:48:56 +0100

    WithEntities

---
+++ MyDbApp/MyDbApp.Domain/Entities/CategoryEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace MyDbApp.Domain
+{
+    public class CategoryEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ MyDbApp/MyDbApp.Domain/Entities/RunEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace MyDbApp.Domain
+{
+    public class RunEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Count { get; set; }
+        public string Place { get; set; }
+    }
+}
